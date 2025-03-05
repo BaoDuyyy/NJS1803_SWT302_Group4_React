f54ee36c52854a76a4b8528ca49bc2946f9431b9
Bảo Duy <duypcbse184173@fpt.edu.vn>
2025-03-05 13:58:46 +0700

    1.	Component Rendering: o	Initial render of todo list o	Rendering of todo items o	Presence of input field and buttons 2.	User Interactions: o	Adding new todos o	Marking todos as complete o	Editing todos o	Deleting todos

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../src/App';
 
 test('adds a new task when addTask is called', () => {
   const { getByPlaceholderText, getByText, queryByText } = render(<App />);
@@ -18,4 +18,23 @@ test('adds a new task when addTask is called', () => {
 
   // Now, the task should be present in the document
   expect(queryByText('New Task')).toBeInTheDocument();
+});
+
+test('marks a task as complete', () => {
+  const { getByPlaceholderText, getByText, queryByText, getByRole } = render(<App />);
+  
+  const input = getByPlaceholderText('Add new todo...');
+  const addButton = getByText('+ Add');
+
+  // Add a new task
+  fireEvent.change(input, { target: { value: 'New Task' } });
+  fireEvent.click(addButton);
+
+  // Mark the task as complete
+  const checkbox = getByRole('checkbox');
+  fireEvent.click(checkbox);
+
+  // The task should now be marked as complete
+  const task = getByText('New Task');
+  expect(task).toHaveClass('text-decoration-line-through');
 });
